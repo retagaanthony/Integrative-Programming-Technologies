86e6f457d81902a6014f656edd0f7a064410e3ed
retagaanthony <retagaromanthony_bsit@plmun.edu.ph>
2025-09-12 14:54:33 +0800

    Add files via upload

---
+++ getmethod/get.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+ <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class = "container result">
+<?php
+		 echo "Bosssiiinnggg!!!, " . $_GET['firstname'] ." ". $_GET['lastname'] . " Kamusta ang buhay buhay. <3";
+
+ ?>
+ </div>
+</body>
+</html>
