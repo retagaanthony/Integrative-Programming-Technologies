3a675350a8d429ecaf07f1176f166f3ced01e2d4
retagaanthony <retagaromanthony_bsit@plmun.edu.ph>
2025-09-12 10:52:34 +0800

    Add files via upload

---
+++ exer3/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+ <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class = "container result">
+<?php
+		 echo "Hello, " . $_GET['username'] . " You are now logged in. <3";
+
+ ?>
+ </div>
+</body>
+</html>
